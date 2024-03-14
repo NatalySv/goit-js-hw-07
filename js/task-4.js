ab7244319ffea734d7cdc8e59e60c445d34dc1f1
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-03-14 22:17:39 +0200

    Add task-4.js
    Add task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const loginForm = document.querySelector('.login-form');
 
+const emailInput = loginForm.querySelector('input[type="email"]');
+emailInput.classList.add('input');
+
+const passwordInput = loginForm.querySelector('input[type="password"]');
+passwordInput.classList.add('input');
+
+const buttonForm = loginForm.querySelector('button[type="submit"]');
+
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const elements = event.target.elements;
+
+  const infoFromForm = {
+    email: elements.email.value,
+    password: elements.password.value,
+  };
+  if (!elements.email.value || !elements.password.value) {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(infoFromForm);
+    loginForm.reset();
+  }
+}
