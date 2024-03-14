bc90e43792fcbdd02ed77552a66dd233a7894a8e
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-03-14 23:43:10 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -2,9 +2,11 @@ const loginForm = document.querySelector('.login-form');
 
 const emailInput = loginForm.querySelector('input[type="email"]');
 emailInput.classList.add('input');
+emailInput.setAttribute('autocomplete', 'off');
 
 const passwordInput = loginForm.querySelector('input[type="password"]');
 passwordInput.classList.add('input');
+passwordInput.setAttribute('autocomplete', 'off');
 
 const buttonForm = loginForm.querySelector('button[type="submit"]');
 
@@ -15,9 +17,10 @@ function handleSubmit(event) {
   const elements = event.target.elements;
 
   const infoFromForm = {
-    email: elements.email.value,
-    password: elements.password.value,
+    email: elements.email.value.trim(),
+    password: elements.password.value.trim(),
   };
+
   if (!elements.email.value || !elements.password.value) {
     alert('All form fields must be filled in');
   } else {
